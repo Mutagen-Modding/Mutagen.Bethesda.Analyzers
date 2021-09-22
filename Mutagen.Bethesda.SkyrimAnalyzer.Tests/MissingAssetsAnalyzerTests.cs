644908a9f10bb8f502e35d1cca10975c9de3067f
erri120 <erri120@protonmail.com>
2021-09-22 16:11:38 +0200

    Add missing assets check for TextureSet

--- Mutagen.Bethesda.SkyrimAnalyzer.Tests/MissingAssetsAnalyzerTests.cs
+++ Mutagen.Bethesda.SkyrimAnalyzer.Tests/MissingAssetsAnalyzerTests.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.IO.Abstractions.TestingHelpers;
-using System.Linq;
 using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
 using Mutagen.Bethesda.Plugins.Records;
@@ -27,8 +26,47 @@ namespace Mutagen.Bethesda.SkyrimAnalyzer.Tests
             Assert.Collection(
                 result.Errors,
                 x => Assert.Equal(MissingAssetsAnalyzer.MissingFemaleArmorModel, x.Message),
-                x => Assert.Equal(MissingAssetsAnalyzer.MissingMaleArmorModel, x.Message)
-                );
+                x => Assert.Equal(MissingAssetsAnalyzer.MissingMaleArmorModel, x.Message));
+        }
+
+        [Fact]
+        public void TestMissingTextureSetTextures()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>());
+            var analyzer = new MissingAssetsAnalyzer(NullLogger<MissingAssetsAnalyzer>.Instance, fileSystem);
+
+            var textureSetRecord = Mock.Of<ITextureSetGetter>();
+            var mock = Mock.Get(textureSetRecord);
+
+            mock.Setup(x => x.Diffuse)
+                .Returns(Path.GetRandomFileName());
+            mock.Setup(x => x.NormalOrGloss)
+                .Returns(Path.GetRandomFileName());
+            mock.Setup(x => x.EnvironmentMaskOrSubsurfaceTint)
+                .Returns(Path.GetRandomFileName());
+            mock.Setup(x => x.GlowOrDetailMap)
+                .Returns(Path.GetRandomFileName());
+            mock.Setup(x => x.Height)
+                .Returns(Path.GetRandomFileName());
+            mock.Setup(x => x.Environment)
+                .Returns(Path.GetRandomFileName());
+            mock.Setup(x => x.Multilayer)
+                .Returns(Path.GetRandomFileName());
+            mock.Setup(x => x.BacklightMaskOrSpecular)
+                .Returns(Path.GetRandomFileName());
+
+            var result = analyzer.AnalyzeRecord(textureSetRecord);
+
+            Assert.Collection(
+                result.Errors,
+                x => Assert.Equal(MissingAssetsAnalyzer.MissingDiffuseTexture, x.Message),
+                x => Assert.Equal(MissingAssetsAnalyzer.MissingNormalOrGlossTexture, x.Message),
+                x => Assert.Equal(MissingAssetsAnalyzer.MissingEnvironmentMaskOrSubsurfaceTintTexture, x.Message),
+                x => Assert.Equal(MissingAssetsAnalyzer.MissingGlowOrDetailMap, x.Message),
+                x => Assert.Equal(MissingAssetsAnalyzer.MissingHeightTexture, x.Message),
+                x => Assert.Equal(MissingAssetsAnalyzer.MissingEnvironmentTexture, x.Message),
+                x => Assert.Equal(MissingAssetsAnalyzer.MissingMultilayerTexture, x.Message),
+                x => Assert.Equal(MissingAssetsAnalyzer.MissingBacklightMaskOrSpecular, x.Message));
         }
 
         private static ArmorModel CreateArmorModel()
--- Mutagen.Bethesda.SkyrimAnalyzer/MissingAssetsAnalyzer.cs
+++ Mutagen.Bethesda.SkyrimAnalyzer/MissingAssetsAnalyzer.cs
@@ -2,18 +2,28 @@
 using Microsoft.Extensions.Logging;
 using Mutagen.Bethesda.Analyzers.SDK.Analyzers;
 using Mutagen.Bethesda.Analyzers.SDK.Result;
+using Mutagen.Bethesda.Plugins;
 using Mutagen.Bethesda.Skyrim;
 
 namespace Mutagen.Bethesda.SkyrimAnalyzer
 {
     [Analyzer]
-    public class MissingAssetsAnalyzer : IMajorRecordAnalyzer<IArmorGetter>
+    public class MissingAssetsAnalyzer : IMajorRecordAnalyzer<IArmorGetter>, IMajorRecordAnalyzer<ITextureSetGetter>
     {
         public string Author => "erri120";
         public string Description => "Finds missing assets.";
 
         public const string MissingFemaleArmorModel = "Missing female armor model file";
         public const string MissingMaleArmorModel = "Missing male armor model file";
+
+        public const string MissingDiffuseTexture = "Missing diffuse texture in TextureSet";
+        public const string MissingNormalOrGlossTexture = "Missing normal or gloss texture in TextureSet";
+        public const string MissingEnvironmentMaskOrSubsurfaceTintTexture = "Missing environment mask or subsurface tint texture in TextureSet";
+        public const string MissingGlowOrDetailMap = "Missing glow or detail map in TextureSet";
+        public const string MissingHeightTexture = "Missing height texture in TextureSet";
+        public const string MissingEnvironmentTexture = "Missing environment texture in TextureSet";
+        public const string MissingMultilayerTexture = "Missing multilayer texture in TextureSet";
+        public const string MissingBacklightMaskOrSpecular = "Missing backlight mask or specular texture in TextureSet";
 
         private readonly ILogger<MissingAssetsAnalyzer> _logger;
         private readonly IFileSystem _fileSystem;
@@ -24,25 +34,41 @@ namespace Mutagen.Bethesda.SkyrimAnalyzer
             _fileSystem = fileSystem;
         }
 
-        public AnalyzerResult AnalyzeRecord(IArmorGetter armorRecord)
+        public AnalyzerResult AnalyzeRecord(IArmorGetter armor)
         {
             var result = new AnalyzerResult();
 
-            var femaleFile = armorRecord.WorldModel?.Female?.Model?.File;
-            if (femaleFile != null)
-            {
-                if (!_fileSystem.File.Exists(femaleFile))
-                    result.AddError(MissingFemaleArmorModel, armorRecord.FormKey);
-            }
+            var femaleFile = armor.WorldModel?.Female?.Model?.File;
+            CheckForMissingAsset(femaleFile, armor.FormKey, result, MissingFemaleArmorModel);
 
-            var maleFile = armorRecord.WorldModel?.Male?.Model?.File;
-            if (maleFile != null)
-            {
-                if (!_fileSystem.File.Exists(maleFile))
-                    result.AddError(MissingMaleArmorModel, armorRecord.FormKey);
-            }
+            var maleFile = armor.WorldModel?.Male?.Model?.File;
+            CheckForMissingAsset(maleFile, armor.FormKey, result, MissingMaleArmorModel);
 
             return result;
+        }
+
+        public AnalyzerResult AnalyzeRecord(ITextureSetGetter textureSet)
+        {
+            var result = new AnalyzerResult();
+
+            CheckForMissingAsset(textureSet.Diffuse, textureSet.FormKey, result, MissingDiffuseTexture);
+            CheckForMissingAsset(textureSet.NormalOrGloss, textureSet.FormKey, result, MissingNormalOrGlossTexture);
+            CheckForMissingAsset(textureSet.EnvironmentMaskOrSubsurfaceTint, textureSet.FormKey, result, MissingEnvironmentMaskOrSubsurfaceTintTexture);
+            CheckForMissingAsset(textureSet.GlowOrDetailMap, textureSet.FormKey, result, MissingGlowOrDetailMap);
+            CheckForMissingAsset(textureSet.Height, textureSet.FormKey, result, MissingHeightTexture);
+            CheckForMissingAsset(textureSet.Environment, textureSet.FormKey, result, MissingEnvironmentTexture);
+            CheckForMissingAsset(textureSet.Multilayer, textureSet.FormKey, result, MissingMultilayerTexture);
+            CheckForMissingAsset(textureSet.BacklightMaskOrSpecular, textureSet.FormKey, result, MissingBacklightMaskOrSpecular);
+
+            return result;
+        }
+
+        private void CheckForMissingAsset(string? path, FormKey formKey, AnalyzerResult result, string error)
+        {
+            if (path == null) return;
+
+            if (!_fileSystem.File.Exists(path))
+                result.AddError(error, formKey);
         }
     }
 }
