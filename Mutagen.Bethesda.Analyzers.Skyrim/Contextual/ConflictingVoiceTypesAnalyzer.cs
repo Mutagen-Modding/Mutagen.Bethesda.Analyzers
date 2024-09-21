ba143b5cce106edb62bfd7058ea93a5797360e6e
Elscrux <nickposer2102@gmail.com>
2024-09-21 11:23:04 +0200

    Pass form links instead of resolved editor ids

--- Mutagen.Bethesda.Analyzers.Skyrim/Contextual/ConflictingVoiceTypesAnalyzer.cs
+++ Mutagen.Bethesda.Analyzers.Skyrim/Contextual/ConflictingVoiceTypesAnalyzer.cs
@@ -8,10 +8,10 @@ namespace Mutagen.Bethesda.Analyzers.Skyrim.Contextual;
 
 public class ConflictingVoiceTypesAnalyzer : IContextualAnalyzer
 {
-    public static readonly TopicDefinition<string?, int, string, string?> NpcsWithSameVoiceType = MutagenTopicBuilder.DevelopmentTopic(
+    public static readonly TopicDefinition<ICellGetter, int, List<IFormLinkGetter<INpcGetter>>, IFormLinkGetter<IVoiceTypeGetter>> NpcsWithSameVoiceType = MutagenTopicBuilder.DevelopmentTopic(
             "NPCs with the same voice type in same cell",
             Severity.Suggestion)
-        .WithFormatting<string?, int, string, string?>("Cell {0} includes {1} npcs {2} with the same voice type {3}");
+        .WithFormatting<ICellGetter, int, List<IFormLinkGetter<INpcGetter>>, IFormLinkGetter<IVoiceTypeGetter>>("Cell {0} includes {1} npcs {2} with the same voice type {3}");
 
     public IEnumerable<TopicDefinition> Topics { get; } = [NpcsWithSameVoiceType];
 
@@ -23,34 +23,30 @@ public class ConflictingVoiceTypesAnalyzer : IContextualAnalyzer
         {
             if (cell.IsExteriorCell()) continue;
 
-            var npcVoiceTypes = new Dictionary<IMajorRecordIdentifier, IFormLinkGetter<IVoiceTypeGetter>>();
+            var npcVoiceTypes = new Dictionary<IFormLinkGetter<INpcGetter>, IFormLinkGetter<IVoiceTypeGetter>>();
             foreach (var placedNpc in cell.GetAllPlaced(param.LinkCache).OfType<IPlacedNpcGetter>())
             {
                 if (!param.LinkCache.TryResolve<INpcGetter>(placedNpc.Base.FormKey, out var npc)) continue;
                 if (!npc.IsUnique()) continue;
                 if (npc.Voice.IsNull) continue;
 
-                npcVoiceTypes.TryAdd(npc, npc.Voice);
+                npcVoiceTypes.TryAdd(npc.ToLink(), npc.Voice);
             }
 
             foreach (var grouping in npcVoiceTypes.GroupBy(x => x.Value))
             {
-                var npcNames = grouping.Select(x => x.Key.EditorID ?? x.Key.FormKey.ToString()).ToList();
-                var count = npcNames.Count;
+                var npcs = grouping.Select(x => x.Key).ToList();
+                var count = npcs.Count;
                 if (count <= 1) continue;
 
-                if (param.LinkCache.TryResolve(grouping.Key, out var voiceType))
-                {
-                    result.AddTopic(
-                        ContextualTopic.Create(
-                            cell,
-                            NpcsWithSameVoiceType.Format(cell.EditorID, count, string.Join(", ", npcNames), voiceType.EditorID)
-                        )
-                    );
-                }
+                result.AddTopic(
+                    ContextualTopic.Create(
+                        cell,
+                        NpcsWithSameVoiceType.Format(cell, count, npcs, grouping.Key)
+                    )
+                );
             }
         }
-
 
         return result;
     }
