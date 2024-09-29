6029b78878bd4b68a176a469fd479c278752b4bc
Elscrux <nickposer2102@gmail.com>
2024-09-30 01:29:28 +0200

    More analyzers

---
+++ Mutagen.Bethesda.Analyzers.Skyrim/Record/Cell/Interior/Settlement/NoMerchantChestLocRefTypeAnalyzer.cs
@@ -0,0 +1,46 @@
+﻿using Mutagen.Bethesda.Analyzers.SDK.Analyzers;
+using Mutagen.Bethesda.Analyzers.SDK.Topics;
+using Mutagen.Bethesda.Skyrim;
+namespace Mutagen.Bethesda.Analyzers.Skyrim.Record.Cell.Interior.Settlement;
+
+public class NoMerchantChestLocRefTypeAnalyzer : IContextualRecordAnalyzer<ICellGetter>
+{
+    public static readonly TopicDefinition<IPlacedObjectGetter> NoMerchantChestLocRefType = MutagenTopicBuilder.DevelopmentTopic(
+            "No Bed Location Reference Type",
+            Severity.Suggestion)
+        .WithFormatting<IPlacedObjectGetter>("{0} is a merchant chest and should have MerchantContainerRefType location reference type");
+
+    public static readonly TopicDefinition<IPlacedObjectGetter> InvalidMerchantChestLocRefType = MutagenTopicBuilder.DevelopmentTopic(
+            "Invalid Bed Location Reference Type",
+            Severity.Error)
+        .WithFormatting<IPlacedObjectGetter>("{0} is not a merchant chest and should not have MerchantContainerRefType location reference type");
+
+    public IEnumerable<TopicDefinition> Topics { get; } = [NoMerchantChestLocRefType, InvalidMerchantChestLocRefType];
+
+    public void AnalyzeRecord(ContextualRecordAnalyzerParams<ICellGetter> param)
+    {
+        var cell = param.Record;
+
+        // Skip non-settlement cells
+        if (!cell.IsSettlementCell(param.LinkCache)) return;
+
+        foreach (var placedObject in cell.GetAllPlaced(param.LinkCache).OfType<IPlacedObjectGetter>())
+        {
+            var isMerchantChest = placedObject.IsMerchantChest(param.LinkCache);
+            var hasLocRefType = placedObject.HasLocationRefType(FormKeys.SkyrimSE.Skyrim.LocationReferenceType.MerchantContainerRefType);
+
+            if (isMerchantChest && !hasLocRefType)
+            {
+                param.AddTopic(
+                    NoMerchantChestLocRefType.Format(placedObject),
+                    x => x);
+            }
+            else if (!isMerchantChest && hasLocRefType)
+            {
+                param.AddTopic(
+                    InvalidMerchantChestLocRefType.Format(placedObject),
+                    x => x);
+            }
+        }
+    }
+}
---
+++ Mutagen.Bethesda.Analyzers.Skyrim/Record/Npc/Unique/NoSleepPackageAnalyzer.cs
@@ -0,0 +1,52 @@
+﻿using Mutagen.Bethesda.Analyzers.SDK.Analyzers;
+using Mutagen.Bethesda.Analyzers.SDK.Topics;
+using Mutagen.Bethesda.Skyrim;
+using Noggog;
+namespace Mutagen.Bethesda.Analyzers.Skyrim.Record.Npc.Unique;
+
+public class NoSleepPackageAnalyzer : IContextualRecordAnalyzer<INpcGetter>
+{
+    public static readonly TopicDefinition NoSleepPackage = MutagenTopicBuilder.DevelopmentTopic(
+            "No Sleep Package",
+            Severity.Warning)
+        .WithoutFormatting("Npc has no sleep package");
+
+    public IEnumerable<TopicDefinition> Topics { get; } = [NoSleepPackage];
+
+    public void AnalyzeRecord(ContextualRecordAnalyzerParams<INpcGetter> param)
+    {
+        var npc = param.Record;
+        if (!npc.IsEligibleForTest()) return;
+
+        // Skip NPCs using templates for AI packages
+        if (npc.Configuration.TemplateFlags.HasFlag(NpcConfiguration.TemplateFlag.AIPackages)) return;
+
+        // Skip innkeepers which are usually active 24/7
+        if (npc.HasFaction(param.LinkCache, editorId => editorId is not null && editorId.Contains("JobInnkeeper", StringComparison.Ordinal))) return;
+
+        var hasSleepPackage = npc.Packages
+            .Select(p => p.TryResolve(param.LinkCache))
+            .NotNull()
+            .Any(HasSleepProcedure);
+
+        if (!hasSleepPackage)
+        {
+            param.AddTopic(
+                NoSleepPackage.Format(),
+                x => x.Packages);
+        }
+
+        bool HasSleepProcedure(IPackageGetter package)
+        {
+            if (package.PackageTemplate.IsNull)
+            {
+                return package.ProcedureTree.Any(x => x.ProcedureType == "Sleep");
+            }
+
+            var template = package.PackageTemplate.TryResolve(param.LinkCache);
+            if (template is null) return false;
+
+            return HasSleepProcedure(template);
+        }
+    }
+}
